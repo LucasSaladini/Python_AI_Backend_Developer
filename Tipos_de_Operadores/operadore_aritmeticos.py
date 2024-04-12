c1b95ab021ce46335726577a6e21a6f214c38906
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2024-04-12 14:16:53 -0300

    Trabalhando com tipos de operadores

---
+++ Tipos_de_Operadores/operadore_aritmeticos.py
@@ -0,0 +1,12 @@
+produto_1 = 20
+produto_2 = 10
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 / produto_2)
+# Divisão inteira
+print(produto_1 // produto_2)
+# Resto da divisão (módulo)
+print(produto_1 % produto_2)
+print(produto_1 * produto_2)
+print(produto_1 ** produto_2)
