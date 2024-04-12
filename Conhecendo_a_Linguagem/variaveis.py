919c7b10cb2569108e12cfb3019b4459717add37
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2024-04-12 11:47:24 -0300

    Utilizando as funções básicas do Python

---
+++ Conhecendo_a_Linguagem/variaveis.py
@@ -0,0 +1,12 @@
+nome = "Lucas"
+idade = 30
+
+nome, idade = ("Jessica", 22)
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+
+print(BRAZILIAN_STATES)
