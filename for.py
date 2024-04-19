e167b7281075a2ee46dc96a26be1d20be26d6175
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2024-04-19 15:43:18 -0300

    Delete for.py

--- for.py
+++
@@ -1,9 +0,0 @@
-texto = input("Informe um texto: ")
-VOGAIS = "AEIOU"
-
-for letra in texto:
-    if letra.upper() in VOGAIS:
-        print(letra, end="")
-else:
-    print()
-    print("Executa no final do laço")
