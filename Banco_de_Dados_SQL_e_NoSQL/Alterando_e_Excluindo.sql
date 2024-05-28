b333289b20d4a1ea65343f56921f56fc7207b046
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2024-05-28 10:14:34 -0300

    Create Alterando_e_Excluindo.sql

---
+++ Banco_de_Dados_SQL_e_NoSQL/Alterando_e_Excluindo.sql
@@ -0,0 +1,15 @@
+CREATE TABLE usuarios_nova (
+  id INT,
+  nome VARCHAR(255) NOT NULL COMMENT 'Nome do usuário',
+  email VARCHAR(255) NOT NULL UNIQUE COMMENT 'Endereço de email do usuário',
+  data_nascimento DATE NOT NULL COMMENT 'Data de nascimento do usuário',
+  endereco VARCHAR(100) NOT NULL COMMENT 'Endereço do cliente'
+  );
+  
+  INSERt INTO usuarios_nova (id, nome, email, endereco, data_nascimento)
+  SELECT id, nome, email, endereco, data_nascimento 
+  FROM usuarios;
+  
+DROP TABLE usuarios;
+
+ALTER TABLE usuarios_nova RENAME usuarios;
