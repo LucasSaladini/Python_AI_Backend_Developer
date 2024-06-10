4894e77b778a4003ba00b141c58b26e893e58bc8
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2024-06-10 19:59:17 +0000

    finalizado a controller de atleta

--- Desenvolvimento_Web/FastAPI/workout_api/atleta/schemas.py
+++ Desenvolvimento_Web/FastAPI/workout_api/atleta/schemas.py
@@ -1,5 +1,7 @@
-from typing import Annotated
+from typing import Annotated, Optional
 from pydantic import BaseModel
+from workout_api.categorias.schemas import CategoriaIn
+from workout_api.centro_treinamento.schemas import CentroTreinamentoAtleta
 
 from workout_api.contrib.schemas import BaseSchema, OutMixin
 
@@ -10,9 +12,15 @@ class Atleta(BaseSchema):
     peso: Annotated[PositiveFloat, Field(description='Peso do atleta', example=75.5)]
     altura: Annotated[PositiveFloat, Field(description='Altura do atleta', example=1.70)]
     sexo: Annotated[str, Field(description='Sexo do atleta', example='M', max_length=1)]
+    categoria: Annotated[CategoriaIn, Field(description='Categoria do atleta')]
+    centro_treinamento: Annotated[CentroTreinamentoAtleta. Field(description='Centro de treinamento do atleta')]
 
 class AtletaIn(Atleta):
     pass
 
 class AtletaOut(Atleta, OutMixin):
     pass
+
+class AtletaUpdate(BaseSchema):
+    nome: Annotated[Optional[str], Field(None, description='Nome do atleta', example='João', max_length=50)]
+    idade: Annotated[Optional[int], Field(None, description='Idade do atleta', example=25)]
