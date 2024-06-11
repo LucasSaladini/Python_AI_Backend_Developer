45fb8ebd6d45748dbff53a8e9ec84eb0eaa48515
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2024-06-11 12:34:26 +0000

    Criado primeiro teste relacionado ao produto

---
+++ Desenvolvimento_Web/TDD/tests/factories.py
@@ -0,0 +1,2 @@
+def product_data():
+    return data: {'name': 'iPhone 14 Pro Max', 'quantity': 10, 'price': 8.500, 'status': True}
---
+++ Desenvolvimento_Web/TDD/tests/schemas/test_product.py
@@ -0,0 +1,30 @@
+from uuid import UUID
+from pydantic import ValidationError
+
+import pytest
+from store.schemas.product import ProductIn
+from tests.factories import product_data
+
+def test_schemas_return_success():
+    data = product_data()
+    product = ProductIn.model_validate(data)
+
+    assert product.name == "iPhone 14 Pro Max"
+    assert isinstance(product.id, UUID)
+
+def tests_schemas_return_raise():
+    data: {
+        'name': 'iPhone 14 Pro Max',
+        'quantity': 10,
+        'price': 8.500,
+    }
+    with pytest.raises(ValidationError) as err:
+        ProductIn.model_validate(data)
+
+        assert err.value.errors()[0] == {
+            "type": "missing",
+            "loc": ("status",),
+            "msg": "Field required",
+            "input": {"name": "iPhone 14 Pro Max", "quantity": 10, "price": 8.5},
+            "url": "https://errors.pydantic.dev/2.5/v/missing"
+        }
